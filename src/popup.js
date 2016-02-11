9ac9b916b38fe5097eb0b84a05c47121bd5682ec
Chris Faria <chrisfaria@gmail.com>
2016-02-10 20:20:26 -0500

    added initial logic to parse weblinks

--- src/popup.js
+++ src/popup.js
@@ -1,15 +1,21 @@
 var defaultOptions;
 
+var travelSites = [
+	"http://wikipedia.org/wiki/{%location}"
+];
+
+const tokenLocation = "{%location}";
+
 document.addEventListener("DOMContentLoaded", function() {
 
-	chrome.runtime.getBackgroundPage(function(eventPage){
+	// chrome.runtime.getBackgroundPage(function(eventPage){
 
 		//initialize the browser action page
 		initializeForm();
 
 		//hook up the search button
 	    document.getElementById("buttonSearch").addEventListener("click", onSearch);
-	});
+	// });
 });
 
 // Ensure that the browser action page is loaded with the last saved values (or defaults)
@@ -20,8 +26,8 @@ function initializeForm(){
 // Search all through a list of websites with the user submitted location
 // Open them in new tabs
 function onSearch() {
-	var location = document.getElementById("textboxLocation").value;
 	
+	var location = document.getElementById("textboxLocation").value;
 	// If the parameters are all valid, proceed with the search
 	if (validateLocation(location)) {
 		searchTravelSites(location);
@@ -31,8 +37,35 @@ function onSearch() {
 // Search the travel sites used by this extension
 // and open a tab for each
 function searchTravelSites(location) {
-	// TODO: Add travel website search
-	alert(location);
+	
+	// Iterate travel sites from array
+	var ctr;
+	for (ctr = 0; ctr < travelSites.length; ++ctr) {
+    	openTravelSiteWithLocation(travelSites[ctr],location);
+	}
+}
+
+// Opens the travel site with the location provided. Each site
+// has a different query strin defined by the travelSites array
+function openTravelSiteWithLocation(travelSite,location) {
+	var webLink = travelSite.replace(tokenLocation,location);
+	console.log(webLink);
+}
+
+function validateLocation(location) {
+	
+	var status = true;
+
+	// Fail if the location is empty
+	if (!location){
+		writeStatusMessage("Please enter a location", true);
+		status = false;
+	} 
+
+	if (status) {
+		writeStatusMessage("", true);
+	}
+	return status;
 }
 
 // Writes a status message to the screen. Informational or error, which get styled with CSS file
@@ -54,20 +87,4 @@ function writeStatusMessage(message, isError) {
       status.textContent = ""; statusError.textContent = "";
     }, timeout);
 
-}
-
-function validateLocation(location) {
-	
-	var status = true;
-
-	// Fail if the location is empty
-	if (!location){
-		writeStatusMessage("Please enter a location", true);
-		status = false;
-	} 
-
-	if (status) {
-		writeStatusMessage("", true);
-	}
-	return status;
 }
